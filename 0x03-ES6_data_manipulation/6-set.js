c44358d22a27a64b9a9cf9df2c07feaa0541c05f
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-02 21:22:58 -0700

    add t6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * setFromArray - creates a Set from an array
+ * @param {Array} array - the array to create
+ * @author rasha saeed <https://github.com/saeed>
+ * @returns a Set containing the elements
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
