1dc60118c8dea4b87ae25dceaff4c7bad512e25a
rashaaz <rashaabdelaziz111@gmail.com>
2024-07-22 00:12:47 -0700

    add

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Function to display a message to the console
+ * @param {String} The message to be displayed.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
